eba505bfe7dac2927c668c8080edb5802636f621
owafa <koyo061130@gmail.com>
2025-12-26 20:06:39 +0900

    攻撃、ドロップアイテム生成など

--- Assets/App/Scripts/Enemy/EnemyStatus.cs
+++ Assets/App/Scripts/Enemy/EnemyStatus.cs
@@ -1,31 +1,37 @@
 using UnityEngine;
 
-// 敵の「ステータス（命）」を管理するクラス
+// 敵のステータスを管理するクラス
 public class EnemyController2
 {
     //変数 
-    private int hp;
+    private EnemyHP hpSystem;
     private Transform transform; // 死亡時に自身を消すために保持
+    private int itemID;
+    private int itemCount;
 
     // HPを外部から確認できるようにする
-    public int CurrentHP => hp;
+    public int CurrentHP => hpSystem.CurrentHP;
 
-    // コンストラクタ:生成時に体(transform)と最大HPをもらう
-    public EnemyController2(Transform selfTransform, int maxHP)
+    // コンストラクタ:生成時に体(transform)と最大HP、アイテムのIDと個数を受け取るようにする
+    public EnemyController2(Transform selfTransform, int maxHP, int itemID, int itemCount)
+    
     {
+        
         this.transform = selfTransform;
-        this.hp = maxHP;
+        this.hpSystem = new EnemyHP(maxHP);
+        this.itemID = itemID;
+        this.itemCount = itemCount;
     }
 
     // ダメージを受ける処理
     public void TakeDamage(int damage)
     {
         // HPを減らす
-        hp -= damage;
-        Debug.Log($"被ダメージ 現在のHP: {hp}");
+        hpSystem.Damage(damage);
+        Debug.Log($"被ダメージ 現在のHP: {hpSystem.CurrentHP}");
 
         // HPが0以下になったら死亡処理
-        if (hp <= 0)
+        if (hpSystem.CurrentHP <= 0)
         {
             Die();
         }
@@ -34,7 +40,12 @@ public class EnemyController2
     // 死亡時の処理
     private void Die()
     {
-        Debug.Log("敵が死亡");
+        //死亡時の座標を取得
+        Vector2 dropPosition = transform.position;
+        //static関数を呼び出してアイテムを生成
+        ItemInstantiater.InstantiateItem(dropPosition, itemID, itemCount);
+
+        Debug.Log($"敵が死亡。ID:{itemID}を{itemCount}個ドロップ");
         //敵を削除する
         Object.Destroy(transform.gameObject);
     }
